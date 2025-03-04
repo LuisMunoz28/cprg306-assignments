00ff046da4cf9c3362e878ef7a6064300b697eb0
Luis Munoz <132930414+LuisMunoz28@users.noreply.github.com>
2025-03-04 14:48:45 -0700

    week6 done

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,3 +1,5 @@
+'use client'
+
 import Item from "./item.js";
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -5,25 +7,43 @@ import { useState } from "react";
 import items from "./items.json";
 
 function ItemList() 
-{
+{   
     const [sortBy, setSortBy] = useState("name");
     
 
     if(sortBy === "name") 
         {
-            items.sort((a,b) => a.name.LocaleCompare(b.name)); 
+            items.sort((a,b) => a.name.localeCompare(b.name)); 
         }
     else if(sortBy === "category") 
         {
-            items.sort((a,b) => a.category.LocaleCompare(b.category));
+            items.sort((a,b) => a.category.localeCompare(b.category));
+        }
+    else if(sortBy === "quantity") 
+        {
+            items.sort((a,b) => a.quantity - b.quantity);
+            items.reverse();
         }
 
     return (
         <div>
+            <button onClick={() => (setSortBy("name"))} 
+            className={sortBy === "name" ? "bg-blue-600 m-1 p-1 rounded-lg" : "bg-blue-400 m-1 p-1 rounded-lg"} 
+            >Sort By Name</button>
+            <button onClick={() => (setSortBy("category"))} 
+            className={sortBy === "category" ? "bg-blue-600 m-1 p-1 rounded-lg" : "bg-blue-400 m-1 p-1 rounded-lg"} 
+            >Sort By Category</button>
+            <button onClick={() => (setSortBy("quantity"))} 
+            className={sortBy === "quantity" ? "bg-blue-600 m-1 p-1 rounded-lg" : "bg-blue-400 m-1 p-1 rounded-lg"} 
+            >Sort By Quantity</button>
+            <ul>
+                {items.map((item) => <Item key={item.id} name={item.name} quantity={item.quantity} category={item.category} />)}
+            </ul>
 
         </div>
     )
 
+    
 }
 
 export default ItemList;
--- app/week-6/item.js
+++ app/week-6/item.js
@@ -5,7 +5,7 @@ function Item({name, quantity, category})
 {
     return(
         <div>
-            <ul className="m-2 p-2 bg-blue-300 w-64 rounded-lg">
+            <ul className="m-2 p-2 bg-blue-800 w-64 rounded-lg">
                 <li>Name: {name}</li>
                 <li>Quantity: {quantity}</li>
                 <li>Category: {category}</li>
